a601094fb40959121371ec3fbe89d4d0627cfa34
unlike1abhinav <dhimanabhinav8295@gmail.com>
2024-11-04 13:16:03 +0530

    loginpage

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import { Logo, FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to='/register'>Register</Link>
-    </div>
-  )
-}
-
-export default Login
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type='email' name='email' defaultValue='john@gmail.com' />
+        <FormRow type='password' name='password' defaultValue='secret123' />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <button type='button' className='btn btn-block'>
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+export default Login;
